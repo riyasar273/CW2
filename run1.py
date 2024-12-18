fc8715c2c1e7ac9b02c68b65489f631838e3a120
rs12g22 <rs12g22@soton.ac.uk>
2024-12-18 17:47:00 +0000

    function to make tiny image

---
+++ run1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def create_tiny_img(path):
+    img = Image.open(path)
+    height = img.height
+    width = img.width
+    cropped_img = img.crop(((width/2)-8,(height/2)-8 , (width/2)+8, (height/2)+8))
+    return cropped_img
